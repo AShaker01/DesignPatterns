1b2f1630430012be6b097b363d7ab61f6745936d
Ahmed Shaker <Ahmed.Shaker@integrant.com>
2021-05-27 02:55:49 +0200

    add head first

---
+++ Head First Design Pattern/Projects/Chapter 1 - Strategy Pattern/DuckApp/Duck.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckApp
+{
+    public abstract class Duck
+    {
+        protected IFlyBehaviour FlyBehaviour;
+        protected IQuackBehaviour QuackBehaviour;
+        public void Swim()
+        {
+            Console.WriteLine("Swim");
+        }
+        public void SetFlyBehaviour(IFlyBehaviour flyBehaviour)
+        {
+            this.FlyBehaviour = flyBehaviour;
+        }
+        public void SetQuackBehaviour(IQuackBehaviour quackBehaviour)
+        {
+            this.QuackBehaviour = quackBehaviour;
+        }
+        public void PerformQuack()
+        {
+            this.QuackBehaviour.SayQuack();
+        }
+
+        public void PerformFly()
+        {
+            this.FlyBehaviour.Fly();
+        }
+
+        public abstract void Display();
+    }
+
+    public class MallodDuck : Duck
+    {
+        public MallodDuck()
+        {
+            this.QuackBehaviour = new Quack();
+            this.FlyBehaviour = new FlyWithWings();
+        }
+        public override void Display()
+        {
+            Console.WriteLine("MallodDuck Display");
+        }
+    }
+
+    public class RedHeadDuck : Duck
+    {
+        public RedHeadDuck()
+        {
+            this.QuackBehaviour = new Squeak();
+            this.FlyBehaviour = new FlyNoWay();
+        }
+        public override void Display()
+        {
+            Console.WriteLine("RedHeadDuck Display");
+        }
+    }
+
+    public interface IFlyBehaviour
+    {
+        void Fly();
+    }
+    public class FlyWithWings : IFlyBehaviour
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Fly With Wings");
+        }
+    }
+
+    public class FlyNoWay : IFlyBehaviour
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Fly NoWay!!");
+        }
+    }
+
+    public interface IQuackBehaviour
+    {
+        void SayQuack();
+    }
+    public class Quack : IQuackBehaviour
+    {
+        public void SayQuack()
+        {
+            Console.WriteLine("Say Quack");
+        }
+    }
+
+    public class Squeak : IQuackBehaviour
+    {
+        public void SayQuack()
+        {
+            Console.WriteLine("Say Squack");
+        }
+    }
+
+}
---
+++ Head First Design Pattern/Projects/Chapter 1 - Strategy Pattern/DuckApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DuckApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Duck mallodDuck = new MallodDuck();
+            mallodDuck.Display();
+            mallodDuck.PerformFly();
+            mallodDuck.PerformQuack();
+
+            // change flying and quack behviour at run time.
+            mallodDuck.SetFlyBehaviour(new FlyNoWay());
+            mallodDuck.SetQuackBehaviour(new Squeak());
+            mallodDuck.PerformFly();
+            mallodDuck.PerformQuack();
+
+            Duck redHeadDuck = new RedHeadDuck();
+            redHeadDuck.Display();
+            redHeadDuck.PerformFly();
+            redHeadDuck.PerformQuack();
+
+            Console.ReadLine();
+        }
+    }
+}
